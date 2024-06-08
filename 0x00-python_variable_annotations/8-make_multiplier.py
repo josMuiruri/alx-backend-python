d703c74c81ab4ec13412af0dea5d95703cb28328
josMuiruri <josemuiruri51@gmail.com>
2024-06-09 02:29:13 +0300

    fixed - return to multiplier_func

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -2,5 +2,5 @@
 from typing import Callable
 def make_multiplier(multiplier: float) -> Callable[[float], float]:
     def multiplier_function(value: float) -> float:
-        return multiplier * value
-    return multiplier
+        return value * multiplier
+    return multiplier_function
