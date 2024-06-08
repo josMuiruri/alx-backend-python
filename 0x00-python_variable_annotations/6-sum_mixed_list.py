4bb29a974623c08e27a86e65c057ea67dbf7537d
josMuiruri <josemuiruri51@gmail.com>
2024-06-09 00:40:05 +0300

    changes - inc Union & del of float from the return

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -1,4 +1,4 @@
 #!/usr/bin/env python3
-from typing import List
-def sum_mixed_list(mxd_lst: List[int, float] -> float:
-        return sum(float(mxd_lst))
+from typing import List, Union
+def sum_mixed_list(mxd_lst: List[Union[int, float]] -> float:
+        return sum(mxd_lst)
