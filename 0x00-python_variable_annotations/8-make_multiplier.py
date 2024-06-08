47fb3a6f553fd17da479c4a861afe74b7871b7c4
josMuiruri <josemuiruri51@gmail.com>
2024-06-09 01:58:46 +0300

    change - func to call func & return value*multiplier

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python3
 from typing import Callable
-def make_multiplier(multiplier: float) -> Callable[float]:
-    return lambda x: multiplier*x
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    def multiplier_function(value: float) -> float:
+        return multiplier * value
+    return multiplier
