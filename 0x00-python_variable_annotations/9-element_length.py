fb7e12e052e66b42f1590011024bf5e587b30eea
josMuiruri <josemuiruri51@gmail.com>
2024-06-09 02:43:59 +0300

    retun a tuple

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
-from typing import Iterator
+from typing import Iterator, Tuple
+
 def element_lenth(lst: int) -> Iterator:
-    return [(i, len(i)) for i in lst]
+    return Tuple[(i, len(i)) for i in lst]
 
