ee38369d46f9324396b80649c2fc4ecdcce0bd10
josMuiruri <josemuiruri51@gmail.com>
2024-06-15 05:30:25 +0300

    changes - func annotation

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -3,9 +3,14 @@
 
 import asyncio
 import random
+from typing import AsyncGenerator
 
-async def async_generator():
-    """ coroutine yield random number btn 0 & 10, ten times,each time waiting 1 second asynchronously """
+
+async def async_generator() -> AsyncGenerator[float, None]:
+    """ coroutine yield random number btn 0 & 10, ten times,each time waiting 1 second asynchronously.
+    Returns:
+        AsyncGenerator[float, None]: An asynchronous generator yielding float values.
+    """
     for _ in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
